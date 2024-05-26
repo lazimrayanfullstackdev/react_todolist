e20e482e747dcb4ddb01adfeb2f8b935e35a4ce9
Lazim Rayan <lazimrayanfreelancer@gmail.com>
2024-05-26 13:12:56 +0600

    Track Event change in input field

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
 function App() {
+
+  var itemType = [];
+  function handleChange(event){
+    const {name, value} = event.target;
+    console.log(value);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input type="text" onChange={handleChange} name="ItemName"/>
         <button>
           <span>Add</span>
         </button>
