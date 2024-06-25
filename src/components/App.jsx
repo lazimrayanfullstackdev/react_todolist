356126ef099a8c0e6362c5111c1ec6d15bf04f1a
Rayan <lazimrayanfreelancer@gmail.com>
2024-06-25 13:20:11 +0600

    Item Added in the array and logged successfully

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,25 +3,29 @@ import React, {useState} from "react";
 
 function App() {
 
-  const [items, setUtem] = useState([]);
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState("");
 
   function handleChange(event){
     const {name, value} = event.target;
     console.log(value);
-
+    setItem(value);
   }
 
   function clicked(){
     //hold the value of name
     console.log("I was clicked!");
+    setItems((prevValue) => [...prevValue,item]);
+    setItem("");
   }
+  console.log(items);
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" onChange={handleChange} name="ItemName"/>
+        <input type="text" onChange={handleChange} name="ItemName" value={item}/>
         <button onClick={clicked}>
           <span>Add</span>
         </button>
